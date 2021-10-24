bc8c5121ea59916edb0fa13e58b2a2c203807b26
Tempest <623059008@qq.com>
2021-10-24 06:05:04 -0400

    fix: change url to cloud ip

--- mintalBackend/config/config.default.js
+++ mintalBackend/config/config.default.js
@@ -20,7 +20,7 @@ module.exports = appInfo => {
         // username
         user: 'root',
         // password
-        password: '',
+        password: 'Art(*)%)%',
         // database
         database: 'mintal',
       },
