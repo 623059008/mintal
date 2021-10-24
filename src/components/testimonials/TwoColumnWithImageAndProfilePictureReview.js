ec25c39a97770902ad7cf58b604402dbbc25fed4
Chanson98 <elaine980418@gmail.com>
2021-10-24 01:55:13 -0400

    up;oad76

--- src/components/testimonials/TwoColumnWithImageAndProfilePictureReview.js
+++ src/components/testimonials/TwoColumnWithImageAndProfilePictureReview.js
@@ -68,7 +68,7 @@ const DecoratorBlob2 = tw(
 export default ({
   subheading = "",
   heading = "Testimonials",
-  description = "Here are what some of our amazing customers are saying about our hotels & tours. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  description = "Here are what some of our amazing customers are saying about our hotels & tours.",
   testimonials = null,
   textOnLeft = false
 }) => {
@@ -83,7 +83,7 @@ export default ({
       profileImageSrc:
         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80",
       quote:
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
+        "Life was, is, will be tough, but toughness cannot completely stop my pace. Mintal gives me courage, a refreshing scent of mint when I felt overworked. Work feels easier when you have a smile.",
       customerName: "Charlotte Hale",
       customerTitle: "CEO, Delos Inc."
     },
