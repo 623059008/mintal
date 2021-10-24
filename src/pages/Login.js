41a8115ab0e3b68a8ff32e929230238d14400fbd
Youyang <fyy623@uw.edu>
2021-10-24 06:55:25 -0400

    typo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -76,7 +76,7 @@ export default ({
   submitButtonText = "Sign In",
   SubmitButtonIcon = LoginIcon,
   forgotPasswordUrl = "#",
-  signupUrl = `/signup`,
+  signupUrl = `/registry`,
   goquiz=()=>{
     vis=1;
   }
